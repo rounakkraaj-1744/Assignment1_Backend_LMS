93c2eb0c8d07b37254f2ce8d2be3b3e643924a1c
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2025-02-15 15:52:24 +0530

    added create-author.dto.ts

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Get, Body } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { CreateAuthorDto } from './dto/create-author.dto';
 
 @Controller('authors')
 export class AuthorController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async createAuthor(@Body() data: { name: string }) {
+  async createAuthor(@Body() data: CreateAuthorDto) {
     return this.prisma.author.create({ data });
   }
 
--- src/author/dto/create-author.dto.ts
+++ src/author/dto/create-author.dto.ts
@@ -3,7 +3,4 @@ import { IsEmail, IsInt, IsString } from "class-validator";
 export class CreateAuthorDto {
     @IsString()
     name: string
-
-    @IsString()
-    books:string[]
 }
